8c62214a4c98b6b14651d3b41214f46ac2c3966c
Sh Kim <sun12334567@gmail.com>
2021-11-14 23:43:34 +0900

    버튼을 누르면 시계 다이얼로그 화면이 나오고, 시간을 선택하고 OK버튼을 누르면 입력한 시간과 분의 값을 Toast로 출력함.

--- app/src/main/java/com/example/project_ex/MainActivity.java
+++ app/src/main/java/com/example/project_ex/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.project_ex;
 import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Dialog;
+import android.app.TimePickerDialog;
 import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
@@ -12,6 +14,9 @@ import android.os.PowerManager;
 import android.provider.Settings;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+import android.widget.TimePicker;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -20,7 +25,25 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
 
+
+    public void timerPicker(View view){
+
+        int hour = 0;
+        int minute = 0;
+
+        TimePickerDialog timePickerDialog = new TimePickerDialog
+                (MainActivity.this, new TimePickerDialog.OnTimeSetListener() {
+                    @Override
+                    public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+
+                        String time = "Hour: " + String.valueOf(hourOfDay) + "\n"
+                                    + "Minute: " + String.valueOf(minute);
+                        Toast.makeText(MainActivity.this, time, Toast.LENGTH_LONG).show();
+                    }
+                },hour, minute, false);
+        timePickerDialog.show();
     }
 
     public void gotoSetting(View view){
@@ -34,18 +57,20 @@ public class MainActivity extends AppCompatActivity {
         // 참고: https://lcw126.tistory.com/277
     }
 
-    public void XgotoSettingX(View view){
+//    public void XgotoSettingX(View view){
+//
+//        Button buttonSetting;
+//
+//        buttonSetting = (Button)findViewById(R.id.setting_btn);
+//
+//        buttonSetting.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Intent intent = new Intent(Settings.ACTION_MANAGE_OVERLAY_PERMISSION);
+//                startActivityForResult(intent, 0);
+//            }
+//        });
+//    }
 
-        Button buttonSetting;
 
-        buttonSetting = (Button)findViewById(R.id.setting_btn);
-
-        buttonSetting.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(Settings.ACTION_MANAGE_OVERLAY_PERMISSION);
-                startActivityForResult(intent, 0);
-            }
-        });
-    }
 }
