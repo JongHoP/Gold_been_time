30ba963cd8603482eb0ecfa453925ea5a355e608
최성진 <81632633+csj0362@users.noreply.github.com>
2021-11-23 13:26:33 +0900

    만보기 및 카운트 다운

---
+++ app/src/main/java/com/example/project_ex/pedometerLockScreen.java
@@ -0,0 +1,237 @@
+package com.example.gold_being_time;
+
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.os.Bundle;
+import android.app.Activity;
+import android.os.Handler;
+import android.os.HandlerThread;
+import android.os.Message;
+import android.os.SystemClock;
+import android.widget.CalendarView;
+import android.widget.Chronometer;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.core.content.ContextCompat;
+
+import com.dinuscxj.progressbar.CircleProgressBar;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+
+
+public class pedometerLockScreen extends Activity implements SensorEventListener { //
+
+    private static final String DEFAULT_PATTERN = "%d"+ "/" +"%d";
+
+    private Thread timeThread = null;
+    TextView myOutput;
+    TextView tv_sensor;
+    TextView goal_count;
+    TextView time;
+    TextView kcal;
+    SensorManager sm;
+    Sensor sensor_step_detector;
+    int steps = 15000;
+    CircleProgressBar circleProgressBar;
+    private static Handler mHandler ;
+    Long ell;  //타이머 시간(초로 계산하여 나옴)
+
+    //스톱워치 상태를 위한 상수
+    final static int RUNNING = 1;
+    final static int STOP = 0;
+    int mStatus = RUNNING;
+    long mBaseTime;
+    long mPauseTime;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        time = findViewById(R.id.tv4);
+        //myOutput = (TextView)findViewById(R.id.tv4);
+        tv_sensor = findViewById(R.id.sensor);
+        tv_sensor.setText("0");  //걸음 수 초기화 및 출력
+
+        sm = (SensorManager)getSystemService(SENSOR_SERVICE);  //센서 매니저 생성
+        sensor_step_detector = sm.getDefaultSensor(Sensor.TYPE_STEP_DETECTOR);  //스텝 감지 센서 등록
+
+        circleProgressBar = findViewById(R.id.cpb_circlebar);
+
+        BackRunnable runnable = new BackRunnable();
+        Thread thread = new Thread(runnable);
+        thread.setDaemon(true);
+        thread.start();
+
+        kcalRunnable runnable1 = new kcalRunnable();
+        Thread thread1 = new Thread(runnable1);
+        thread1.setDaemon(true);
+        thread1.start();
+
+        circleBar();  //원형 프로세스 바
+        timerOn();  //타이머
+    }
+
+    //원형 프로세스바
+    public void circleBar(){
+        goal_count = (TextView)findViewById(R.id.tv2);  //목표 값 가져오기
+        int num = Integer.parseInt(goal_count.getText().toString());
+        CircleProgressBar.ProgressFormatter progressFormatter = new CircleProgressBar.ProgressFormatter() {
+            @Override
+            public CharSequence format(int progress, int max) {
+                return String.format(DEFAULT_PATTERN,steps, num);
+            }
+        };
+        circleProgressBar.setProgressFormatter(progressFormatter);
+        circleProgressBar.setProgressTextColor(ContextCompat.getColor(this, R.color.button_color));
+        circleProgressBar.setProgressBackgroundColor(ContextCompat.getColor(this, R.color.white_gray_color));
+        circleProgressBar.setProgressStartColor(ContextCompat.getColor(this, R.color.button_color));
+        circleProgressBar.setProgressEndColor(ContextCompat.getColor(this, R.color.button_color));
+        int max = 100;
+        int progress = 70;
+        int pi = 360;
+        int start = 270;
+        int degree = start-(int)((steps/(double)num)*pi);
+        circleProgressBar.setMax(num);
+        circleProgressBar.setProgress(steps);
+        circleProgressBar.setStartDegree(degree);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        sm.registerListener(this, sensor_step_detector, SensorManager.SENSOR_DELAY_NORMAL );
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        sm.unregisterListener(this);
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent sensorEvent) {  //센서값이 변할 때
+        switch(sensorEvent.sensor.getType()){  //센서 유형이 스텝 감지 센서인 경우 걸음수를 1증가하여 출력력
+            case Sensor.TYPE_STEP_DETECTOR:
+                tv_sensor.setText(""+ (++steps));
+                break;
+        }
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int i) {
+    }
+
+
+    //스톱워치에 사용하는 핸들러
+    Handler handler = new Handler(){
+        @Override
+        public void handleMessage(android.os.Message msg) {
+
+            time.setText(getEllapse()); //텍스트뷰 수정
+            handler.sendEmptyMessage(0);  //메시지 다시 보냄(0은 메시지 구분 위ㅏㅁ)
+            allStop();
+
+        }
+    };
+
+    @Override
+    protected void onDestroy() {
+        handler.removeMessages(0);  //메시지 지워서 메모리누수 방지
+        super.onDestroy();
+    }
+
+    public void timerOn(){
+        switch(mStatus){
+            case RUNNING:  //시작상태이면
+                mBaseTime = SystemClock.elapsedRealtime();  //현재값 세팅
+                handler.sendEmptyMessage(0);  //헨들러로 메시지 보냄
+                System.out.println("러닝 반복");
+                //mStatus = STOP;
+                break;
+            /*case STOP:
+                long now = SystemClock.elapsedRealtime();
+                mBaseTime += (now - mPauseTime);
+                handler.sendEmptyMessage(0);
+                mStatus = RUNNING;
+                break;*/
+            case STOP:
+                handler.removeMessages(0);
+
+                //처음상태로 원상복귀시킴
+                time.setText(String.format("%02d:%02d:%02d", ell / 1000 / 60 / 60, ell / 1000 / 60, (ell/1000)%60));
+                //mStatus = RUNNING;
+                break;
+        }
+    }
+
+    //타이머 시간 계산
+    public String getEllapse(){
+        long now = SystemClock.elapsedRealtime();
+        ell = now - mBaseTime;   //현재 시간과 지난 시간을 빼서 ell값을 구함함
+       String sEll = String.format("%02d:%02d:%02d", ell / 1000 / 60 / 60, ell / 1000 / 60, (ell/1000)%60);
+        return sEll;
+    }
+
+    public void allStop(){  //시간 멈추기
+        if(ell > 5000){
+            mStatus = STOP;
+            timerOn();
+        }else
+            mStatus = RUNNING;
+    }
+
+
+
+
+
+    public void calKcal(){
+        kcal = findViewById(R.id.tv6);
+        double met;
+        met = 3.3* 3.5 * 70 * (ell/1000/60); //3.3 * 3.5 * 70 * ell;
+        kcal.setText((int) met*5);
+    }
+
+
+
+
+
+    class BackRunnable implements Runnable{
+        private boolean stopped=false;
+        @Override
+        public void run() {
+            while(!stopped){   //!Thread.currentThread().isInterrupted()
+                circleBar();
+                try{
+                    Thread.sleep(1000);
+                }catch(InterruptedException e){
+                    e.printStackTrace();
+                }
+            }
+        }
+        public void stop(){
+            stopped=true;
+        }
+    }
+
+    class kcalRunnable implements Runnable{
+        private boolean stopped=false;
+        @Override
+        public void run() {
+            while(!stopped){   //!Thread.currentThread().isInterrupted()
+
+                try{
+                    Thread.sleep(1000);
+                }catch(InterruptedException e){
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+}
---
+++ app/src/main/java/com/example/project_ex/timerLockScreen.java
@@ -0,0 +1,156 @@
+package com.example.gold_being_time;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.Nullable;
+import androidx.core.content.ContextCompat;
+
+import com.dinuscxj.progressbar.CircleProgressBar;
+
+public class timerLockScreen extends Activity  {
+
+    private static final String DEFAULT_PATTERN = "%d"+ ":" +"%d" + ":" + "%d" ;
+
+    CountDownTimer countDownTimer;
+    TextView splitGoal; //입력받은 시간 텍스트뷰
+    String goalNum;  //시간을 숫자로 쪼갠 것
+    TextView ex;
+
+    //String hour, min, second;
+
+    CircleProgressBar circleProgressBar;
+
+    String exnum="000010";
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.timer_lock_screen);
+
+        splitGoal = (TextView) findViewById(R.id.tv2);
+        circleProgressBar = findViewById(R.id.cpb_circlebar);
+
+        //splitGoal();
+        countDown(exnum);
+        circleBar();
+    }
+
+    //입력받은 시간을 숫자와 문자로 나누기
+    public String splitGoal(){
+
+        String goal = splitGoal.getText().toString();
+        if(goal.indexOf("시간")>0){
+            goalNum = goal.substring(0, goal.indexOf("시간"));
+
+        }else if(goal.indexOf("분")>0){
+            goalNum = goal.substring(0, goal.indexOf("분"));
+        }
+        Toast.makeText(this, goalNum, Toast.LENGTH_SHORT).show();
+        return goalNum;
+    }
+
+    public void countDown(String time){
+        ex = (TextView) findViewById(R.id.tv3);
+        long conversionTime = 0;
+
+        // 1000 단위가 1초
+        // 60000 단위가 1분
+        // 60000 * 3600 = 1시간
+
+        String getHour = time.substring(0, 2);
+        String getMin = time.substring(2, 4);
+        String getSecond = time.substring(4, 6);
+
+        // "00"이 아니고, 첫번째 자리가 0 이면 제거
+        if (getHour.substring(0, 1) == "0") {
+            getHour = getHour.substring(1, 2);
+        }
+
+        if (getMin.substring(0, 1) == "0") {
+            getMin = getMin.substring(1, 2);
+        }
+
+        if (getSecond.substring(0, 1) == "0") {
+            getSecond = getSecond.substring(1, 2);
+        }
+
+        // 변환시간
+        conversionTime = Long.valueOf(getHour) * 1000 * 3600 + Long.valueOf(getMin) * 60 * 1000 + Long.valueOf(getSecond) * 1000;
+
+        // 첫번쨰 인자 : 원하는 시간 (예를들어 30초면 30 x 1000(주기))
+        // 두번쨰 인자 : 주기( 1000 = 1초)
+        countDownTimer = new CountDownTimer(conversionTime, 1000) {
+            // 특정 시간마다 뷰 변경
+            public void onTick(long millisUntilFinished) {
+
+                // 시간단위
+                String hour = String.valueOf(millisUntilFinished / (60 * 60 * 1000));
+
+                // 분단위
+                long getMin = millisUntilFinished - (millisUntilFinished / (60 * 60 * 1000)) ;
+                String min = String.valueOf(getMin / (60 * 1000)); // 몫
+
+                // 초단위
+                String second = String.valueOf((getMin % (60 * 1000)) / 1000); // 나머지
+
+                // 시간이 한자리면 0을 붙인다
+                if (hour.length() == 1) {
+                    hour = "0" + hour;
+                }
+
+                // 분이 한자리면 0을 붙인다
+                if (min.length() == 1) {
+                    min = "0" + min;
+                }
+
+                // 초가 한자리면 0을 붙인다
+                if (second.length() == 1) {
+                    second = "0" + second;
+                }
+                ex.setText(hour + ":" + min + ":" + second);
+
+            }
+
+            // 제한시간 종료시
+            public void onFinish() {
+                // 변경 후
+                ex.setText("촬영종료!");
+                // TODO : 타이머가 모두 종료될때 어떤 이벤트를 진행할지
+            }
+        }.start();
+    }
+
+    
+    
+    public void circleBar(){
+/*        splitGoal = (TextView)findViewById(R.id.tv2);  //목표 값 가져오기
+        int num = Integer.parseInt(splitGoal.getText().toString());*/
+        CircleProgressBar.ProgressFormatter progressFormatter = new CircleProgressBar.ProgressFormatter() {
+            @Override
+            public CharSequence format(int progress, int max) {
+
+                return String.format(DEFAULT_PATTERN, countDownTimer., min, second);   //위의 hour, min, second 값을 못 읽어옴
+            }
+        };
+
+        circleProgressBar.setProgressFormatter(progressFormatter);
+        circleProgressBar.setProgressTextColor(ContextCompat.getColor(this, R.color.button_color));
+        circleProgressBar.setProgressBackgroundColor(ContextCompat.getColor(this, R.color.white_gray_color));
+        circleProgressBar.setProgressStartColor(ContextCompat.getColor(this, R.color.button_color));
+        circleProgressBar.setProgressEndColor(ContextCompat.getColor(this, R.color.button_color));
+        int max = 100;
+        int progress = 70;
+        int pi = 360;
+        int start = 270;
+
+        int degree = start-(int)((Integer.parseInt("100")/(double)Double.parseDouble(exnum))*pi);
+        circleProgressBar.setMax(Integer.parseInt(exnum));
+        circleProgressBar.setProgress(1);
+        circleProgressBar.setStartDegree(degree);
+    }
+}
+
