2da8388970ddbddc2f94b7aa75b9e655eafba477
최성진 <81632633+csj0362@users.noreply.github.com>
2021-12-05 20:49:56 +0900

    etc

--- TestSplash/app/src/main/java/com/example/testsplash/Complete.java
+++ TestSplash/app/src/main/java/com/example/testsplash/Complete.java
@@ -1,17 +1,24 @@
 package com.example.testsplash;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
-
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.TextView;
 
 
 public class Complete extends AppCompatActivity {
 
-
+    Animation animation;
+    TextView textView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         setTheme(R.style.AppTheme);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.complete);
+
+        textView = findViewById(R.id.tt1);
+        animation = AnimationUtils.loadAnimation(getApplicationContext(),R.anim.blink_text);
+        textView.startAnimation(animation);
     }
 
 
